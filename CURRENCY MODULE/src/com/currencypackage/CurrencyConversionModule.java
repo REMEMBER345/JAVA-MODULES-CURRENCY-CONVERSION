b54ac214af4f8885879be07433f7bcd8a330a1a9
REMEMBER345 <rememberjoselyne345@gmail.com>
2024-11-21 19:20:22 +0300

    #commit2
    Added a maven framework to the project
    imported JUnit and Mockito libraries

---
+++ CURRENCY MODULE/src/com/currencypackage/CurrencyConversionModule.java
@@ -0,0 +1,12 @@
+package com.currencypackage; //package declaration
+//necessary JUnit and Mockito packages to ensure unit testing and mocking dependencies respectively.
+import static org.junit.jupiter.api.Assertions.*; // imports static assertions from JUnit5
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+public class CurrencyConversionModule {
+}
