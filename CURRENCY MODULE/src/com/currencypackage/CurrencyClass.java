76523b358d6c4bb16efae535c903733537f1fe1a
REMEMBER345 <rememberjoselyne345@gmail.com>
2024-11-25 11:27:13 +0300

    #commit5
    Created a mock object for the Currency Class and declared a default exchange rate to cater for currency conversion incase the external service is at fault

--- CURRENCY MODULE/src/com/currencypackage/CurrencyClass.java
+++ CURRENCY MODULE/src/com/currencypackage/CurrencyClass.java
@@ -1,13 +1,18 @@
 
 package com.currencypackage; //package declaration
 ////necessary JUnit and Mockito packages to ensure unit testing and mocking dependencies respectively.
-//import static org.junit.jupiter.api.Assertions.*; // imports static assertions from JUnit5
-//import static org.mockito.Mockito.*;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.time.LocalDateTime;
-public class CurrencyClass {
+import static org.junit.jupiter.api.Assertions.*; // imports static assertions from JUnit5
+import static org.mockito.Mockito.*;//imports Mockito methods like when and verify for mocking behavior in unit tests.
+import org.junit.jupiter.api.BeforeEach;//Provides the @BeforeEach annotation to set up test-specific configurations before each test.
+import org.junit.jupiter.api.Test;//Allows marking methods as unit tests using the @Test annotation.
+import org.mockito.Mock;//Marks a variable as a mock object (a simulated external dependency).
+import org.mockito.MockitoAnnotations;//Initializes mock objects
+import java.time.LocalDateTime;//Enables working with timestamps for audit logs.
+public class CurrencyClass { //Declares the test class for CurrencyConversionModule. It contains all the unit tests.
+     CurrencyConversion conversionModule; //test module
+    @Mock
+    private ExternalServiceRate mockExternalServiceRate; //Declares a mock object of ExternalRateService to simulate fetching exchange rates.
+    static final double CurrentExchangeRate = 1.0;
+    //Specifies a fallback exchange rate used when the external service is unavailable.
+    
 }
